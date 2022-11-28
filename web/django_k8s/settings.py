4a5b42c41d905baa2aaa8ada41d26823e078c4ff
Juan Felipe <juan.fll@hotmail.com>
2022-11-27 21:57:59 -0300

    aprendendo kubernets com digitalocean

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -87,6 +87,7 @@ DB_IS_AVAILABLE = all([
     DB_PORT
 ])
 
+DB_IGNORE_SSL=os.environ.get("DB_IGNORE_SSL") == 'true'
 POSTGRES_READY = str(os.environ.get("POSTGRES_READY")) == "1"
 
 if DB_IS_AVAILABLE and POSTGRES_READY:
@@ -100,7 +101,11 @@ if DB_IS_AVAILABLE and POSTGRES_READY:
             "HOST": DB_HOST,
         }
     }
-    print(DATABASES)
+    if not DB_IGNORE_SSL:
+        DATABASES["default"]["OPTIONS"] = {
+            "sslmode" : "required"
+        }
+   
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
