f3b6d4ede4cf67d0c52e933b34657b3d0aaaa8d9
Juan Felipe <juan.fll@hotmail.com>
2022-12-04 13:49:00 -0300

    corrigindo docker-compose para local environment

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #thirdy party
     'storages',
+    #internal apps
+    'posts',
 ]
 
 MIDDLEWARE = [
