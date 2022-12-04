2d81a48b57478a23e32b3212559f934e2be1f298
Juan Felipe <juan.fll@hotmail.com>
2022-12-04 17:00:41 -0300

    finalizando projeto com test de hello world

--- web/posts/tests.py
+++ web/posts/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
 class PostTestCase(TestCase):
-    
+
+    def setUp(self):
+        Post.objects.create(title="Hello world")
+        
 
     def test_failure(self):
-        self.assertTrue(False)
+        queryset = Post.objects.all()
+        self.assertTrue(queryset.exists())
